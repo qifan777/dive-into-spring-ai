dd309578aa894cdeb248768fb9066549ff36e3b0
linjiacheng <1507906763@qq.com>
2024-06-21 09:38:03 +0800

    sql

--- database.sql
+++ database.sql
@@ -26,8 +26,6 @@ CREATE TABLE `ai_function` (
   `id` varchar(36) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
   `created_time` datetime(6) NOT NULL,
   `edited_time` datetime(6) NOT NULL,
-  `creator_id` varchar(32) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
-  `editor_id` varchar(32) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
   `name` varchar(32) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL COMMENT '函数名称',
   `description` varchar(1000) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL COMMENT '函数描述',
   PRIMARY KEY (`id`)
@@ -40,7 +38,7 @@ CREATE TABLE `ai_function` (
 
 LOCK TABLES `ai_function` WRITE;
 /*!40000 ALTER TABLE `ai_function` DISABLE KEYS */;
-INSERT INTO `ai_function` VALUES ('4703133f93874c51bdc7860cc781632f','2024-06-08 15:27:29.806904','2024-06-08 15:39:58.594491','dcd256e2412f4162a6a5fcbd5cfedc84','dcd256e2412f4162a6a5fcbd5cfedc84','springSpELFunction','执行符合SpringSpE语法的表达式');
+INSERT INTO `ai_function` VALUES ('a58148f6b12547f5823911935e3fb6f1','2024-06-20 21:56:39.632048','2024-06-20 21:56:39.632048','documentAnalyzerFunction','文档解析函数');
 /*!40000 ALTER TABLE `ai_function` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -59,7 +57,7 @@ CREATE TABLE `ai_message` (
   `editor_id` varchar(32) NOT NULL,
   `type` varchar(32) NOT NULL COMMENT '消息类型(用户/助手/系统)',
   `text_content` text NOT NULL COMMENT '消息内容',
-  `medias` json DEFAULT NULL,
+  `medias` json DEFAULT NULL COMMENT '媒体内容如图片链接、语音链接',
   `ai_session_id` varchar(32) NOT NULL COMMENT '会话id',
   PRIMARY KEY (`id`),
   KEY `ai_message_ai_session_id_fk` (`ai_session_id`)
@@ -72,45 +70,9 @@ CREATE TABLE `ai_message` (
 
 LOCK TABLES `ai_message` WRITE;
 /*!40000 ALTER TABLE `ai_message` DISABLE KEYS */;
-INSERT INTO `ai_message` VALUES ('099c0d014bdf4a4da693d6c3e5eee9c0','2024-06-18 14:33:54.049532','2024-06-18 14:33:54.049532','fe3d0d7d6eb34eb7a6b7985426cf8af7','fe3d0d7d6eb34eb7a6b7985426cf8af7','USER','我的名字是起凡','[]','5e7542f85bd247ffba192cbc7cd06c5e');
-INSERT INTO `ai_message` VALUES ('0e0603e02f0e42ec83bd4afa41f319a9','2024-06-18 14:33:57.006347','2024-06-18 14:33:57.006347','fe3d0d7d6eb34eb7a6b7985426cf8af7','fe3d0d7d6eb34eb7a6b7985426cf8af7','ASSISTANT','很高兴很高兴认识你很高兴认识你，起凡！有什么很高兴认识你，起凡！有什么我能帮到你的吗？','[]','5e7542f85bd247ffba192cbc7cd06c5e');
-INSERT INTO `ai_message` VALUES ('4c93364c696d45ea903817bddc3f8450','2024-06-18 14:27:07.877939','2024-06-18 14:27:07.877939','fe3d0d7d6eb34eb7a6b7985426cf8af7','fe3d0d7d6eb34eb7a6b7985426cf8af7','ASSISTANT','你好你好！有什么你好！有什么我能为你效劳的你好！有什么我能为你效劳的吗？','[]','5e7542f85bd247ffba192cbc7cd06c5e');
-INSERT INTO `ai_message` VALUES ('9f0de4712575484fa050c2475453fa1f','2024-06-18 14:34:00.989601','2024-06-18 14:34:00.989601','fe3d0d7d6eb34eb7a6b7985426cf8af7','fe3d0d7d6eb34eb7a6b7985426cf8af7','USER','我叫什么名字？','[]','5e7542f85bd247ffba192cbc7cd06c5e');
-INSERT INTO `ai_message` VALUES ('d2cfbb52381c4cd090c0d030994a2d85','2024-06-18 14:27:05.442812','2024-06-18 14:27:05.442812','fe3d0d7d6eb34eb7a6b7985426cf8af7','fe3d0d7d6eb34eb7a6b7985426cf8af7','USER','你好','[]','5e7542f85bd247ffba192cbc7cd06c5e');
-INSERT INTO `ai_message` VALUES ('ee44cb8d7a3544e189e6e6516a25c16a','2024-06-18 14:34:02.368642','2024-06-18 14:34:02.368642','fe3d0d7d6eb34eb7a6b7985426cf8af7','fe3d0d7d6eb34eb7a6b7985426cf8af7','ASSISTANT','你你叫你叫起你叫起凡。','[]','5e7542f85bd247ffba192cbc7cd06c5e');
+INSERT INTO `ai_message` VALUES ('1d14d513ae8c4ffda0fb50df6e24ba77','2024-06-20 22:08:28.389711','2024-06-20 22:08:28.389711','fe3d0d7d6eb34eb7a6b7985426cf8af7','fe3d0d7d6eb34eb7a6b7985426cf8af7','USER','C:\\Users\\Administrator\\Desktop\\2023年工作总结及2024年工作展望-林家成.docx，这份文档的内容是什么？','[]','c4d192c4b2de48bcae585f5b9672cd1d');
+INSERT INTO `ai_message` VALUES ('ba161500bec84b388a6501b3be333b45','2024-06-20 22:08:28.649439','2024-06-20 22:08:28.649439','fe3d0d7d6eb34eb7a6b7985426cf8af7','fe3d0d7d6eb34eb7a6b7985426cf8af7','ASSISTANT','2023年的工作总结及2024年工作展望文档内容概要如下：\n\n1、**工作回顾重点：**\n   - 成功运用Python脚本在一经上云项目中实现Oracle脚本批量迁移、实体批量登记等，极大提升了工作效率（速度提升70倍以上），并提前完成年度工作任务。\n   - 领导新智慧工厂项目的前端与后端框架搭建，采用更高效、安全的技术栈，促进团队快速投入业务开发。重构并优化了基础功能模块，如菜单框架、租户管理等，封装为易用组件。\n   - 开发了脚本依赖分析工具，通过SQL语法树解析等技术，有效辅助故障检测、代码规范检查及开发效率，与部门其他系统（如指标库、报表系统）集成，增强功能实用性。\n\n2、**个人成长与进步：**\n   - 今年最大的成就是深入学习了数据开发知识，综合软件开发能力使自己更能满足用户需求，相比同行具有更全面的技能组合。\n\n3、**应对挑战的方法：**\n   - 面对SQL语法解析等高难度任务，通过自学源码、复习理论基础，成功开发出工具，体现了自我驱动力和解决问题的能力。\n\n4、**反思与改进空间：**\n   - 希望能在项目中实践深度学习、机器学习技术，这是未来努力的一个方向。\n\n5、**提质增效的建议：**\n   - 强调代码审查和个人技能提升，鼓励使用先进技术和工具，以及创建持续学习的环境，以保持竞争力。\n\n6、**对当前开发工作的建议：**\n   - 提倡技术分享会议，打破知识孤岛，促进新老员工间的技术交流和学习，平衡稳定与创新的需求。\n\n7、**其他建议：**\n   - 加强跨部门合作，以新智慧工厂项目为范例，推动更多跨领域协作成果。\n\n对于2024年的展望虽未详细展开，但基于上述总结，可能包括深化技术应用、促进团队协作、继续个人技能升级等方面。',NULL,'c4d192c4b2de48bcae585f5b9672cd1d');
 /*!40000 ALTER TABLE `ai_message` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `ai_role`
---
-
-DROP TABLE IF EXISTS `ai_role`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `ai_role` (
-  `id` varchar(36) NOT NULL,
-  `created_time` datetime(6) NOT NULL,
-  `edited_time` datetime(6) NOT NULL,
-  `creator_id` varchar(32) NOT NULL,
-  `editor_id` varchar(32) NOT NULL,
-  `name` varchar(255) NOT NULL COMMENT '角色名称',
-  `description` varchar(32) NOT NULL,
-  `icon` varchar(255) DEFAULT NULL COMMENT '图标',
-  `prompts` json NOT NULL COMMENT '预置提示词',
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `name` (`name`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `ai_role`
---
-
-LOCK TABLES `ai_role` WRITE;
-/*!40000 ALTER TABLE `ai_role` DISABLE KEYS */;
-INSERT INTO `ai_role` VALUES ('87dde7fd46b24b9c8836dd1d48348307','2024-06-01 21:07:10.219035','2024-06-01 21:07:10.219035','dcd256e2412f4162a6a5fcbd5cfedc84','dcd256e2412f4162a6a5fcbd5cfedc84','python助手','可以帮助你编写python代码','https://my-community.oss-cn-qingdao.aliyuncs.com/20240601210542起凡.jpg','[{\"type\": \"SYSTEM\", \"content\": [{\"text\": \"你是一个python助手可以帮助用户编写python程序\"}]}]');
-/*!40000 ALTER TABLE `ai_role` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
@@ -137,7 +99,9 @@ CREATE TABLE `ai_session` (
 
 LOCK TABLES `ai_session` WRITE;
 /*!40000 ALTER TABLE `ai_session` DISABLE KEYS */;
-INSERT INTO `ai_session` VALUES ('5e7542f85bd247ffba192cbc7cd06c5e','2024-06-18 14:01:21.242508','2024-06-18 14:01:21.241948','fe3d0d7d6eb34eb7a6b7985426cf8af7','fe3d0d7d6eb34eb7a6b7985426cf8af7','新的聊天');
+INSERT INTO `ai_session` VALUES ('b495186b9259494792a9fe4ce8807f25','2024-06-18 22:01:51.459293','2024-06-18 22:01:51.459293','fe3d0d7d6eb34eb7a6b7985426cf8af7','fe3d0d7d6eb34eb7a6b7985426cf8af7','新的聊天');
+INSERT INTO `ai_session` VALUES ('c4d192c4b2de48bcae585f5b9672cd1d','2024-06-18 22:01:45.819825','2024-06-18 22:01:45.819317','fe3d0d7d6eb34eb7a6b7985426cf8af7','fe3d0d7d6eb34eb7a6b7985426cf8af7','新的聊天');
+INSERT INTO `ai_session` VALUES ('dcd32fd9f7fc4d0fb9a5b7dc9ff0dbc8','2024-06-18 21:57:09.390651','2024-06-18 21:57:09.390651','fe3d0d7d6eb34eb7a6b7985426cf8af7','fe3d0d7d6eb34eb7a6b7985426cf8af7','新的聊天');
 /*!40000 ALTER TABLE `ai_session` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -182,4 +146,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2024-06-18 15:12:28
+-- Dump completed on 2024-06-21  9:37:48
